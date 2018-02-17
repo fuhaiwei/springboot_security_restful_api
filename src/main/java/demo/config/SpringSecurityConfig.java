9a9bdf4b97008bf04f1157300aa936f9ed2ddcfe
fuhaiwei <fuhaiwei@126.com>
2018-02-17 18:10:44 +0800

    refactor: Use @Autowired

--- src/main/java/demo/config/SpringSecurityConfig.java
+++ src/main/java/demo/config/SpringSecurityConfig.java
@@ -6,6 +6,7 @@ import demo.action.SessionController;
 import demo.security.CustomAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,10 +16,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -30,6 +35,15 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private CustomLoginHandler customLoginHandler;
+
+    @Autowired
+    private CustomLogoutHandler customLogoutHandler;
+
+    @Autowired
+    private CustomAccessDeniedHandler customAccessDeniedHandler;
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
@@ -47,10 +61,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutUrl("/api/session/logout")
-                .logoutSuccessHandler(new LogoutHandler())
+                .addLogoutHandler(customLogoutHandler)
+                .logoutSuccessHandler(customLogoutHandler)
                 .and()
                 .exceptionHandling()
-                .authenticationEntryPoint(new AuthenticationFailureHandler());
+                .accessDeniedHandler(customAccessDeniedHandler)
+                .authenticationEntryPoint(customAccessDeniedHandler);
 
         http.csrf()
                 .ignoringAntMatchers("/api/session/**");
@@ -62,31 +78,41 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private CustomAuthenticationFilter customAuthenticationFilter() throws Exception {
         CustomAuthenticationFilter filter = new CustomAuthenticationFilter();
-        filter.setAuthenticationSuccessHandler(new LoginSuccessHandler());
-        filter.setAuthenticationFailureHandler(new LoginFailureHandler());
-        filter.setAuthenticationManager(authenticationManagerBean());
+        filter.setAuthenticationSuccessHandler(customLoginHandler);
+        filter.setAuthenticationFailureHandler(customLoginHandler);
+        filter.setAuthenticationManager(authenticationManager());
         filter.setFilterProcessesUrl("/api/session/login");
         return filter;
     }
 
-    private static class AuthenticationFailureHandler extends BaseController implements AuthenticationEntryPoint {
+    @Component
+    public static class CustomAccessDeniedHandler extends BaseController implements AuthenticationEntryPoint, AccessDeniedHandler {
+        // NoLogged Access Denied
         @Override
         public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
             response.getWriter().write(errorMessage(authException.getMessage()));
             response.flushBuffer();
         }
+
+        // Logged Access Denied
+        @Override
+        public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+            response.getWriter().write(errorMessage(accessDeniedException.getMessage()));
+            response.flushBuffer();
+        }
     }
 
-    private class LoginSuccessHandler extends BaseController implements AuthenticationSuccessHandler {
+    @Component
+    public static class CustomLoginHandler extends BaseController implements AuthenticationSuccessHandler, AuthenticationFailureHandler {
+        // Login Success
         @Override
         public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
             LOGGER.info("User login successfully, name={}", authentication.getName());
             response.getWriter().write(objectResult(SessionController.getJSON(authentication)));
             response.flushBuffer();
         }
-    }
 
-    private class LoginFailureHandler extends BaseController implements org.springframework.security.web.authentication.AuthenticationFailureHandler {
+        // Login Failure
         @Override
         public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
             response.getWriter().write(errorMessage(exception.getMessage()));
@@ -94,7 +120,15 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         }
     }
 
-    private class LogoutHandler extends BaseController implements LogoutSuccessHandler {
+    @Component
+    public static class CustomLogoutHandler extends BaseController implements LogoutHandler, LogoutSuccessHandler {
+        // Before Logout
+        @Override
+        public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+
+        }
+
+        // After Logout
         @Override
         public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
             response.getWriter().write(objectResult(SessionController.getJSON(null)));
