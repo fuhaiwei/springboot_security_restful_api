6db49b3444f9104459f8f10ea7c4ba13d6e1fe26
fuhaiwei <fuhaiwei@126.com>
2020-05-09 23:47:17 +0800

    Use React Router

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from "react-router-dom"
+import App from './App'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  , document.getElementById('root'))
