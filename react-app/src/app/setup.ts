4f8a474fdbffdab0879f021049a68f7d6fc9b5c7
fuhaiwei <fuhaiwei@126.com>
2022-05-06 20:24:49 +0800

    Update setup.ts

--- react-app/src/app/setup.ts
+++ react-app/src/app/setup.ts
@@ -1,8 +1,6 @@
 import dayjs from 'dayjs'
-import 'dayjs/locale/zh-cn'
 import relativeTime from 'dayjs/plugin/relativeTime'
 
 export default function setup() {
   dayjs.extend(relativeTime)
-  dayjs.locale('zh-cn')
 }
