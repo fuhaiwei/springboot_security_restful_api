0d7ff2f01b1a897b953088c256abde56e8d1d189
fuhaiwei <fuhaiwei@126.com>
2022-05-08 08:39:12 +0800

    Update error handler

--- src/main/java/com/mingzuozhibi/commons/base/BaseController.java
+++ src/main/java/com/mingzuozhibi/commons/base/BaseController.java
@@ -2,19 +2,30 @@ package com.mingzuozhibi.commons.base;
 
 import com.mingzuozhibi.commons.domain.Result;
 import com.mingzuozhibi.commons.domain.ResultPage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@Slf4j
 public abstract class BaseController extends BaseSupport {
 
     protected static final String MEDIA_TYPE = MediaType.APPLICATION_JSON_VALUE;
 
     @ResponseBody
     @ExceptionHandler
-    public String errorHandler(Exception e) {
+    public String errorHandler(Exception e) throws Exception {
+        if (e instanceof AccessDeniedException) {
+            throw e;
+        }
+        if (e instanceof AuthenticationException) {
+            throw e;
+        }
+        log.warn("errorHandler", e);
         return errorResult(e.toString());
     }
 
--- src/main/java/com/mingzuozhibi/configs/SecurityHandler.java
+++ src/main/java/com/mingzuozhibi/configs/SecurityHandler.java
@@ -1,5 +1,6 @@
 package com.mingzuozhibi.configs;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
@@ -13,22 +14,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
 
 import static com.mingzuozhibi.commons.base.BaseSupport.errorResult;
 
+@Slf4j
 @Component
 public class SecurityHandler implements AuthenticationEntryPoint, AccessDeniedHandler {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request, response));
+        log.info("No login access：{} {}", request.getMethod(), request.getServletPath());
+        logRequestHeaders(request);
         responseText(response, errorResult("You must be logged in to access these resources"));
     }
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request, response));
+        log.info("Unauthorized access：{} {}", request.getMethod(), request.getServletPath());
+        logRequestHeaders(request);
         responseText(response, errorResult("You do not have sufficient permissions to access these resources"));
+    }
+
+    private void logRequestHeaders(HttpServletRequest request) {
+        Enumeration<String> names = request.getHeaderNames();
+        while (names.hasMoreElements()) {
+            String name = names.nextElement();
+            log.debug("{}: {}", name, request.getHeader(name));
+        }
     }
 
     public static void responseText(HttpServletResponse response, String content) throws IOException {
